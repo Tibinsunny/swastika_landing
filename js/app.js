fab193bddc0097e2412b4e85fa08852cabc13021
tibinsunny95 <tibinsunny95@gmail.com>
2020-05-28 12:52:29 -0500

    added intersectionAPI

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+  screen=document.querySelector("h4")
+  observer = new IntersectionObserver(function(test)
+  {
+     
+      if(test[0].intersectionRatio>0)
+      {
+        test[0].target.style.opacity ="1";
+        document.querySelector("h5").style.opacity="1"
+      
+      }
+      else
+      {test[0].target.style.opacity ="0";
+      document.querySelector("h5").style.opacity="0"
+
+      }
+  })
+   observer.observe(screen)
