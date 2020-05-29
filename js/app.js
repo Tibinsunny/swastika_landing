3540e9018962a184740ceb2c744d7f646df6a345
tibinsunny95 <tibinsunny95@gmail.com>
2020-05-29 13:05:24 -0500

    added Nav

--- js/app.js
+++ js/app.js
@@ -8,12 +8,17 @@
         test[0].target.style.opacity ="1";
         document.querySelector("h5").style.opacity="1"
         document.querySelector(".editor").style.opacity="1"
+        document.querySelector(".second_page").style.color="green"
+        document.querySelector(".first_page").style.color="white"
+        
       
       }
       else
       {test[0].target.style.opacity ="0";
       document.querySelector("h5").style.opacity="0"
       document.querySelector(".editor").style.opacity="0"
+      document.querySelector(".second_page").style.color="white"
+      document.querySelector(".first_page").style.color="green"
 
       }
   })
